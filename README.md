83d32c00198348e888222d1311e7776cf22c3174
Marcin Miazga <miazgamarcin@outlook.com>
2018-12-25 20:01:18 +0100

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Outloud.Api
+[![Build Status](https://travis-ci.org/out-loud/Outloud.Api.svg?branch=master)](https://travis-ci.org/out-loud/Outloud.Api)
+## .NET Core Webapi
---
+++ src/Program.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using NGate;
+using System.Threading.Tasks;
+
+namespace Outloud.Api
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+            => await WebHost.CreateDefaultBuilder(args).UseNGate().Build().RunAsync();
+    }
+}
